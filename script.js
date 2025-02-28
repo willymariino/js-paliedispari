211eab630c37cfeddd8deb6f3ac5e9e4d1c69453
willymariino <wg.mariino@gmail.com>
2025-02-28 17:54:14 +0100

    let newstring

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const normalWord = (prompt('inserisci una parola'))
 function reverseWord(normalWord) {
     let newstring = '';
 
-    for (let i = 0; normalWord - 1; i--) {
+    for (let i = normalWord.lenght - 1; i >= 0; i--) {
         newstring = newstring + normalWord[i]
     }
 
