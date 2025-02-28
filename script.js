11101a70a5c135eedc5b72db4b694b995f5b2977
willymariino <wg.mariino@gmail.com>
2025-02-28 18:52:31 +0100

    palindrom verifier finished

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const normalWord = (prompt('inserisci una parola'))
 function reverseWord(normalWord) {
     let newstring = '';
 
-    for (let i = normalWord.lenght - 1; i >= 0; i--) {
+    for (let i = normalWord.length - 1; i >= 0; i--) {
         newstring = newstring + normalWord[i]
     }
 
     return newstring
 
 }
-let newstring = normalWord
+let newstring = reverseWord(normalWord)
 console.log(newstring)
+
+function isWordaPalindrome(newstring) {
+    if (normalWord === newstring) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
+
+
+}
+let result = isWordaPalindrome(newstring);
+console.log(result);
