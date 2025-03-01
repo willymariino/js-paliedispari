3216017d7807c3253c5c9450151bd7bfa53cdc23
willymariino <wg.mariino@gmail.com>
2025-03-01 15:52:43 +0100

    other try

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,9 +1,15 @@
 const userNumber = parseInt(prompt('inserisci un numero'));
 const validNumber = !isNaN(userNumber);
+console.log(userNumber)
 const userGuess = (prompt('inserisci la parola "pari" oppure "dispari" '));
-const validGuess = !ispari(userGuess)
+console.log(userGuess)
 
-function cpuNumber(userNumber)
-let num1 = Math.round(Math.random() * 4) + 1; {
 
+function cpuNumber(userNumber) {
+    let computerNumber = '';
+    let cpuNumber = Math.round(Math.random() * 4) + 1; {
+        computerNumber = computerNumber + cpuNumber
+    }
+    return cpuNumber
 }
+console.log(computerNumber)
