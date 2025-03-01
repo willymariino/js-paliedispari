cb18c0091b2190618a1fa107330e14f9a617f7d5
willymariino <wg.mariino@gmail.com>
2025-03-01 16:26:45 +0100

    added sum numbers

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -5,6 +5,10 @@ const userGuess = (prompt('inserisci la parola "pari" oppure "dispari" '));
 console.log(userGuess)
 
 let num1 = Math.round(Math.random() * 4) + 1; {
-    console.log(num1) // non capisco perchè non stampa in console il numero random
+    console.log(num1) // è uguale all'altro algoritmo, non so perchè questo funziona e stampa in console, mentre l'altro non stampava.s
+}
+
+let sum = userNumber + num1; {
+    console.log(sum)
 }
 
