117a335cd34fe28374963195584231ce607d13ea
willymariino <wg.mariino@gmail.com>
2025-03-01 15:21:34 +0100

    shifted to folder palindrome exercise

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,4 +1,7 @@
-const userNumber = parseInt(prompt('inserisci un numero'))
+const userNumber = parseInt(prompt('inserisci un numero'));
+const validNumber = !isNaN(userNumber);
+const userGuess = (prompt('inserisci la parola "pari" oppure "dispari" '));
+const validGuess = !ispari(userGuess)
 
 function cpuNumber(userNumber)
 let num1 = Math.round(Math.random() * 4) + 1; {
