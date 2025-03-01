0450d517dd345c60ddbe3a1ad0e9cdcea55ac100
willymariino <wg.mariino@gmail.com>
2025-03-01 15:58:18 +0100

    added comment

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -5,6 +5,6 @@ const userGuess = (prompt('inserisci la parola "pari" oppure "dispari" '));
 console.log(userGuess)
 
 let cpuNumber = Math.round(Math.random() * 4) + 1; {
-    console.log(cpuNumber)
+    console.log(cpuNumber) // non capisco perchè non stampa in console il numero random
 }
 
